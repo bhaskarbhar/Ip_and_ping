cc40a30a77bd959a901266e53a526f85b7bdf8df
bhaskarbhar <126053496+bhaskarbhar@users.noreply.github.com>
2024-01-07 19:25:30 +0530

    Add files via upload

--- ip_and_ping.py
+++ ip_and_ping.py
@@ -1,38 +1,41 @@
 from tkinter import *
 from tkinter import messagebox
 import subprocess
-    
+
 def get_ip():
-    ip_bytes = subprocess.check_output(['curl', 'ifconfig.me', '-s'])
-    ip = ip_bytes.decode('utf-8')
-    messagebox.showinfo("Your device' s ip","Ip: " + ip)
+    try:
+        ip_bytes = subprocess.check_output(['curl', 'ifconfig.me', '-s'])
+        ip = ip_bytes.decode('utf-8')
+        messagebox.showinfo("Your device's IP", "IP: " + ip)
+    except subprocess.CalledProcessError:
+        messagebox.showerror("Error", "Unable to fetch IP address.")
 
 window = Tk()
-window.title("Network tools")
+window.title("IP and Ping Finder")
 window.geometry("650x650")
-window.configure(bg = "#b83ba1")
-button_ip= Button(window, text="Get your IP address", command= get_ip)
-button_ip.configure(font= 100)
-button_ip.place(x=230,y=250)
+window.configure(bg="#b83ba1")
 
-label_ping = Label(window, text = "Enter any ip \nor website name: ")
-label_ping.configure(bg = "#b83ba1", font = 5)
-label_ping.place(x=60,y=310)
+button_ip = Button(window, text="Get your IP address", command=get_ip)
+button_ip.configure(font=100)
+button_ip.place(x=230, y=250)
 
-ping_entry = Entry(window, font = 5)
+label_ping = Label(window, text="Enter any IP \nor website name:")
+label_ping.configure(bg="#b83ba1", font=5)
+label_ping.place(x=60, y=310)
+
+ping_entry = Entry(window, font=5)
 ping_entry.place(x=230, y=330)
-global entry_ping
-entry_ping = str(ping_entry.get())
-
 
 def get_ping():
-    
-    ping_output = subprocess.check_output(["ping", entry_ping])
-    messagebox.showinfo("Ping data",ping_output)
+    try:
+        entry_ping = ping_entry.get()
+        ping_output = subprocess.check_output(["ping", entry_ping])
+        messagebox.showinfo("Ping data", ping_output.decode('utf-8'))
+    except subprocess.CalledProcessError as e:
+        messagebox.showerror("Error", f"Unable to ping {entry_ping}.\nError: {e}")
 
-    
-button_ping= Button(window, text="Ping any ip or website", command = get_ping)
-button_ping.configure(font= 100)
-button_ping.place(x=230,y=380)
+button_ping = Button(window, text="Ping any IP or website", command=get_ping)
+button_ping.configure(font=100)
+button_ping.place(x=230, y=380)
 
 window.mainloop()
